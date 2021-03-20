4292f0f37375af93d6bd1c2a620d90a859b8ba29
cxinya <30631351+cxinya@users.noreply.github.com>
2021-03-20 12:23:50 -0400

    scrape s1 queens

--- R/rvest-queen-list.R
+++ R/rvest-queen-list.R
@@ -1,14 +1,31 @@
 library(rvest)
 
-seasons <- seq(1, 13, 1)
+# All regular seasons
+seasons <- seq(1, 2, 1)
 
-html <- str_glue("https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_{i})")
+# Create empty tibble
+all_queens <- tibble(season = numeric(), queen = character())
 
-i <- 1
+for (i in seasons) {
 
-season <- read_html(html)
+  # URL for each season
+  url <- str_glue("https://rupaulsdragrace.fandom.com/wiki/RuPaul%27s_Drag_Race_(Season_{i})")
 
-season %>% html_element(".wikitable") %>%
-  html_table() %>%
-  filter(row_number() > 1) %>%
-  mutate(season = i) %>% View()
+  # Read season's wiki page
+  season <- read_html(url)
+
+  # Read table of queens per season
+  season_queens <- season %>% html_element(".wikitable") %>%
+    html_table() %>%
+    filter(row_number() > 1) %>%
+    select(queen = Contestant) %>%
+    mutate(season = i)
+
+  # Add season to all_queens
+  all_queens <- all_queens %>% rbind(season_queens)
+
+}
+
+# Clean names
+clean_name = str_replace(Contestant, ' "(.*?)"', ""), # Removes nicknames in quotes, like Victoria "Porkchop" Parker
+url_end = str_replace_all(clean_name, " ", "_"),
