8c102aca9491e92673b846e79337636db02bac56
cxinya <30631351+cxinya@users.noreply.github.com>
2021-03-20 20:54:50 -0400

    try row_bind()

--- R/rvest-queen-list.R
+++ R/rvest-queen-list.R
@@ -1,11 +1,14 @@
 library(rvest)
+library(tidyverse)
+library(gtools)
 
 # All regular seasons
-seasons <- seq(1, 2, 1)
+seasons <- seq(1, 13, 1)
 
 # Create empty tibble
-all_queens <- tibble(season = numeric(), queen = character())
+all_queens <- tibble(season = numeric(), rank = character(), queen = character())
 
+# Add queens from each season
 for (i in seasons) {
 
   # URL for each season
@@ -14,18 +17,28 @@ for (i in seasons) {
   # Read season's wiki page
   season <- read_html(url)
 
+  # Solution to turn <br> into spaces: https://stackoverflow.com/questions/30921626/can-rvest-keep-inline-html-tags-such-as-br-using-html-table
+  xml_find_all(season, ".//br") %>% xml_add_sibling("p", "\n")
+  xml_find_all(season, ".//br") %>% xml_remove()
+
   # Read table of queens per season
   season_queens <- season %>% html_element(".wikitable") %>%
     html_table() %>%
-    filter(row_number() > 1) %>%
-    select(queen = Contestant) %>%
-    mutate(season = i)
+    # filter(row_number() > 1) %>%
+    # select(queen = Contestant, rank = Rank) %>%
+    mutate(
+      season = i,
+      col_subtitle = ifelse(row_number() == 1, 1, NA))
 
   # Add season to all_queens
-  all_queens <- all_queens %>% rbind(season_queens)
+  # all_queens <- all_queens %>% rbind(season_queens)
+  all_queens <- all_queens %>% bind_rows(season_queens)
 
 }
 
-# Clean names
-clean_name = str_replace(Contestant, ' "(.*?)"', ""), # Removes nicknames in quotes, like Victoria "Porkchop" Parker
-url_end = str_replace_all(clean_name, " ", "_"),
+
+# # TODO clean names, fix spaces, replace hyphens
+#
+# # Clean names
+# clean_name = str_replace(Contestant, ' "(.*?)"', ""), # Removes nicknames in quotes, like Victoria "Porkchop" Parker
+# url_end = str_replace_all(clean_name, " ", "_"),
